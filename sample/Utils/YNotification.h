cd96127aa03ea97abcbcdffaf204a2583b6eab3c
咸光金 <yong531315@gmail.com>
2020-03-19 16:11:43 +0800

    request notification

---
+++ sample/Utils/YNotification.h
@@ -0,0 +1,17 @@
+//
+//  YNotification.h
+//  sample
+//
+//  Created by ysnows on 2020/3/19.
+//  Copyright © 2020 ysnows. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YNotification : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ sample/Utils/YNotification.m
@@ -0,0 +1,13 @@
+//
+//  YNotification.m
+//  sample
+//
+//  Created by ysnows on 2020/3/19.
+//  Copyright © 2020 ysnows. All rights reserved.
+//
+
+#import "YNotification.h"
+
+@implementation YNotification
+
+@end
